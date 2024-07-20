17430960ea43937c003367398e813e9fbbabedb3
jeffcct <jsmi607@aucklanduni.ac.nz>
2024-07-20 22:28:37 +1200

    added Canvas-resize, a program that shows a canvas over the entire screen. It updates when the screen is resized.

---
+++ README.md
@@ -0,0 +1,5 @@
+canvas-resize:
+- I learnt how to resize a canvas.
+- Originally I had very little experience with javascript, so I had to learn how to query for the canvas object and use object properties to change the size.
+- This, however resulted in a static width change, it would not update if you resized the window. This is not very useful.
+- However I added an actionListener to resize the canvas whenever the window is resized.
---
+++ canvas-resize/canvas.js
@@ -0,0 +1,20 @@
+var canvas = document.querySelector('canvas'); // this is how to find the canvas object
+// This is only called once at the beginning of the program, so it will not update with changes.
+canvas.width = window.innerWidth; 
+canvas.height = window.innerHeight;
+
+context = canvas.getContext('2d');
+
+//To make it work permanantly, we add an eventListener to check if the window size is ever changed. If it is, then we call the resizeCanvas function that fixes the width.
+function initialize() {
+    window.addEventListener('resize', resizeCanvas, false);
+
+    resizeCanvas();
+}
+
+function resizeCanvas() {
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight * 2;    
+}
+
+initialize();
